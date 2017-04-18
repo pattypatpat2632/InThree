95c9defb9dddb6819d1de46292cbb66dda17541d
pattypatpat2632 <oleary.audio@gmail.com>
2017-04-17 21:55:39 -0400

    refactor sequencerView

--- InThree/SequencerVC.swift
+++ InThree/SequencerVC.swift
@@ -10,47 +10,13 @@ import UIKit
 
 class SequencerVC: UIViewController {
     
-    let beat1View = BeatView()
-    let beat2View = BeatView()
-    let beat3View = BeatView()
-    let beat4View = BeatView()
+    
     var sequencerEngine = SequencerEngine()
+    var sequencerView = SequencerView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.night
-        
-        self.view.addSubview(beat1View)
-        beat1View.translatesAutoresizingMaskIntoConstraints = false
-        beat1View.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        beat1View.topAnchor.constraint(equalTo: self.topLayoutGuide.bottomAnchor).isActive = true
-        beat1View.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-        beat1View.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/5).isActive = true
-        beat1View.backgroundColor = UIColor.night
-        
-        self.view.addSubview(beat2View)
-        beat2View.translatesAutoresizingMaskIntoConstraints = false
-        beat2View.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        beat2View.topAnchor.constraint(equalTo: beat1View.bottomAnchor).isActive = true
-        beat2View.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-        beat2View.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/5).isActive = true
-        beat2View.backgroundColor = UIColor.night
-        
-        self.view.addSubview(beat3View)
-        beat3View.translatesAutoresizingMaskIntoConstraints = false
-        beat3View.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        beat3View.topAnchor.constraint(equalTo: beat2View.bottomAnchor).isActive = true
-        beat3View.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-        beat3View.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/5).isActive = true
-        beat3View.backgroundColor = UIColor.night
-        
-        self.view.addSubview(beat4View)
-        beat4View.translatesAutoresizingMaskIntoConstraints = false
-        beat4View.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
-        beat4View.topAnchor.constraint(equalTo: beat3View.bottomAnchor).isActive = true
-        beat4View.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-        beat4View.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/5).isActive = true
-        beat4View.backgroundColor = UIColor.night
+        self.view = sequencerView
         
         sequencerEngine.setUpSequencer()
     }
---
+++ InThree/SequencerView.swift
@@ -0,0 +1,64 @@
+//
+//  SequencerView.swift
+//  InThree
+//
+//  Created by Patrick O'Leary on 4/17/17.
+//  Copyright © 2017 Patrick O'Leary. All rights reserved.
+//
+
+import UIKit
+
+class SequencerView: UIView {
+    
+    let beat1View = BeatView()
+    let beat2View = BeatView()
+    let beat3View = BeatView()
+    let beat4View = BeatView()
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUpViews()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpViews()
+    }
+    
+    func setUpViews() {
+        self.backgroundColor = UIColor.night
+        
+        self.addSubview(beat1View)
+        beat1View.translatesAutoresizingMaskIntoConstraints = false
+        beat1View.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        beat1View.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        beat1View.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        beat1View.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1/5).isActive = true
+        beat1View.backgroundColor = UIColor.night
+        
+        self.addSubview(beat2View)
+        beat2View.translatesAutoresizingMaskIntoConstraints = false
+        beat2View.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        beat2View.topAnchor.constraint(equalTo: beat1View.bottomAnchor).isActive = true
+        beat2View.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        beat2View.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1/5).isActive = true
+        beat2View.backgroundColor = UIColor.night
+        
+        self.addSubview(beat3View)
+        beat3View.translatesAutoresizingMaskIntoConstraints = false
+        beat3View.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        beat3View.topAnchor.constraint(equalTo: beat2View.bottomAnchor).isActive = true
+        beat3View.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        beat3View.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1/5).isActive = true
+        beat3View.backgroundColor = UIColor.night
+        
+        self.addSubview(beat4View)
+        beat4View.translatesAutoresizingMaskIntoConstraints = false
+        beat4View.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+        beat4View.topAnchor.constraint(equalTo: beat3View.bottomAnchor).isActive = true
+        beat4View.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
+        beat4View.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 1/5).isActive = true
+        beat4View.backgroundColor = UIColor.night
+    }
+
+}
