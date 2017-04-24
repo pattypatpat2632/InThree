42f0c314a415aa4964c1c967f73a3df52983d68f
pattypatpat2632 <oleary.audio@gmail.com>
2017-04-24 14:31:04 -0400

    add working note buttons

--- InThree/CircleOfFifthsView.swift
+++ InThree/CircleOfFifthsView.swift
@@ -7,10 +7,30 @@
 //
 
 import UIKit
+import AudioKit
 
 class CircleOfFifthsView: UIView {
 
     @IBOutlet var contentView: UIView!
+    
+    @IBOutlet weak var c: NoteButton!
+    @IBOutlet weak var g: NoteButton!
+    @IBOutlet weak var d: NoteButton!
+    @IBOutlet weak var a: NoteButton!
+    @IBOutlet weak var e: NoteButton!
+    @IBOutlet weak var b: NoteButton!
+    @IBOutlet weak var fS: NoteButton!
+    @IBOutlet weak var cS: NoteButton!
+    @IBOutlet weak var gS: NoteButton!
+    @IBOutlet weak var dS: NoteButton!
+    @IBOutlet weak var aS: NoteButton!
+    @IBOutlet weak var f: NoteButton!
+    
+    
+    var noteButtons = [NoteButton]()
+    
+    
+    
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -25,12 +45,24 @@ class CircleOfFifthsView: UIView {
     func commonInit() {
         Bundle.main.loadNibNamed("CircleOfFifthsView", owner: self, options: nil)
         
+        self.backgroundColor = UIColor.clear
+        self.contentView.backgroundColor = UIColor.clear
+        
         self.addSubview(contentView)
         contentView.translatesAutoresizingMaskIntoConstraints = false
         contentView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
         contentView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
         contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
         contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        
+        noteButtons = [c, cS, d, dS, e, f, fS, g, gS, a, aS, b]
+        var noteCount: MIDINoteNumber = 60
+        noteButtons = noteButtons.map { (noteButton) -> NoteButton in
+            noteButton.noteValue = noteCount
+            noteCount += 1
+            
+            return noteButton
+        }
     }
     
     
---
+++ InThree/NoteButton.swift
@@ -0,0 +1,36 @@
+//
+//  NoteButton.swift
+//  InThree
+//
+//  Created by Patrick O'Leary on 4/24/17.
+//  Copyright © 2017 Patrick O'Leary. All rights reserved.
+//
+
+import UIKit
+import AudioKit
+
+class NoteButton: UIButton {
+    
+    var noteValue: MIDINoteNumber = 60
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    func commonInit() {
+        self.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        self.layer.cornerRadius = self.bounds.width/5
+        self.backgroundColor = UIColor.flash
+    }
+    
+    func buttonTapped() {
+        print("NOTE BUTTON TAPPED WITH NOTE VALUE : \(self.noteValue)")
+    }
+
+}
