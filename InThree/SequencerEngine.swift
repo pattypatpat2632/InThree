43e86d5537c4df69ee30f4cf06a89550ccc8c0bb
pattypatpat2632 <oleary.audio@gmail.com>
2017-04-25 17:43:14 -0400

    commit before merge with master

--- InThree/SequencerEngine.swift
+++ InThree/SequencerEngine.swift
@@ -46,14 +46,6 @@ struct SequencerEngine {
         sequencer.setTempo(newTempo)
     }
     
-    func generateTestSequence() {
-        sequencer.setLength(AKDuration(beats: 8.0))
-        sequencer.tracks[0].clear()
-        for i in 0...15 {
-            sequencer.tracks[0].add(noteNumber: 60, velocity: 127, position: AKDuration(beats: Double(i)/2), duration: AKDuration(beats: 0.5))
-        }
-    }
-    
     func generateSequence(fromScore score: Score) {
         print("*****Generate Sequence*****")
         sequencer.tracks[0].clear()
@@ -67,24 +59,6 @@ struct SequencerEngine {
             }
             beatPostion = beatPostion + AKDuration(beats: 1.0)
         }
-    }
-    
-    func createTestScore(){
-        var beat1 = Beat(rhythm: .two)
-        beat1.notes[0].noteNumber = 60
-        beat1.notes[0].noteOn = true
-        beat1.notes[0].velocity = 127
-        beat1.notes[1].noteNumber = 61
-        beat1.notes[1].noteOn = true
-        beat1.notes[1].velocity = 127
-        
-        var beat2 = Beat(rhythm: .one)
-        beat2.notes[0].noteNumber = 63
-        beat2.notes[0].noteOn = true
-        beat2.notes[0].velocity = 127
-        
-        let score = Score(beats: [beat1, beat2])
-        generateSequence(fromScore: score)
     }
     
 }
