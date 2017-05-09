6f7fc1419a4e4a2ba9fbbf9817ea34cae529e0db
pattypatpat2632 <oleary.audio@gmail.com>
2017-05-09 18:22:14 -0400

    add new bliplabel and blipbutton

---
+++ InThree/BlipLabel.swift
@@ -0,0 +1,21 @@
+//
+//  BlipLabel.swift
+//  InThree
+//
+//  Created by Patrick O'Leary on 5/9/17.
+//  Copyright © 2017 Patrick O'Leary. All rights reserved.
+//
+
+import UIKit
+
+class BlipLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
